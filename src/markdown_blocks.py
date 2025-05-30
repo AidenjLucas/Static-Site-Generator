2f18f9a34c4d9aecef6bfbadb79a0a0fcf10904b
Aidenjlucas <Aidenjlucas.work@gmail.com>
2025-05-30 11:30:54 -0400

    Fixed Markdown Blocks Func / Unit Test

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,9 +1,4 @@
 def markdown_to_blocks(markdown):
-    blocks = list(filter(lambda block: block != "",
+    return list(filter(lambda block: block != "",
                         map(lambda block: block.strip(), markdown.split("\n\n"))))
    
-    for i in range(len(blocks)):
-            tmp =  blocks[i].partition("\n")
-            blocks[i] = tmp[0] + tmp[1] + tmp[2].strip()
-    
-    return blocks
