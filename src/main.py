aa9e6706aed70e0fdded07ec4773ad039c4b8ba5
Aidenjlucas <Aidenjlucas.work@gmail.com>
2025-06-05 13:52:34 -0400

    Added Copy Files from Source To Destination Directory Function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
 from textnode import *
+import os
+import shutil
 
 def main():
+    copy_files("./static","./public")
     return 0
+
+def copy_files(src,dst):
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    else:
+        shutil.rmtree(dst)
+        os.mkdir(dst)
+    
+    for file in os.listdir(src):
+        src_path = os.path.join(src, file)
+        dst_path = os.path.join(dst, file)
+       
+        if os.path.isfile(src_path):
+            shutil.copy(src_path,dst)
+        else:
+            os.mkdir(dst_path)
+            copy_files(src_path,dst_path)
+
+
 
 if __name__ == "__main__":
     main()
+
+
